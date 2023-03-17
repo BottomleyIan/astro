a309f839a5b0cae27e2cb0424b5bf807fe689873
BottomleyIan <bottomley.ian@gmail.com>
2023-03-17 07:50:16 +0000

    adds repos

---
+++ plugins/neodim.lua
@@ -0,0 +1,18 @@
+-- dims unused functions
+return {
+  "zbirenbaum/neodim",
+  event = "LspAttach",
+  opts = {
+    alpha = 0.75,
+    blend_color = "#000000",
+    update_in_insert = {
+      enable = true,
+      delay = 100,
+    },
+    hide = {
+      virtual_text = true,
+      signs = true,
+      underline = true,
+    },
+  },
+}
